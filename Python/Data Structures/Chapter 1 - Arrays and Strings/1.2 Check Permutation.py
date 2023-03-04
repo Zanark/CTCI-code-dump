2f2c5cad182c7ee98416e4c407879f82ac57b925
Debashish Mishra <mishrad@microsoft.com>
2023-03-04 18:38:02 +0530

    Added a O(n) solution for 1.2

---
+++ Python/Data Structures/Chapter 1 - Arrays and Strings/1.2 Check Permutation.py
@@ -0,0 +1,24 @@
+def isUnique(inputString1, inputString2):
+    if len(inputString1) != len(inputString2):
+        return False
+    combinedCharSet = {}
+    for c in inputString1:
+        if c in combinedCharSet:
+            combinedCharSet[c] += 1
+        else:
+            combinedCharSet[c] = 1
+    for c in inputString2:
+        if (c in combinedCharSet):
+            combinedCharSet[c] -= 1
+    for freq in combinedCharSet.values():
+        if freq == 0:
+            continue
+        else:
+            return False
+    return True
+    
+
+if __name__ == '__main__':
+    inputString1 = input("Enter string1 >> \t")
+    inputString2 = input("\nEnter string2 >> \t")
+    print("Are the two strings permutations of each other? >>\t" + str(isUnique(inputString1, inputString2)))
