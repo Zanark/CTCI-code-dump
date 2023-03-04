f768b4765f04d1525f562b6133b23e4de27c821b
Debashish Mishra <mishrad@microsoft.com>
2023-03-04 18:04:25 +0530

    added O(n) solution for 1.1

---
+++ Python/Data Structures/Chapter 1 - Arrays and Strings/1.1 Is Unique.py
@@ -0,0 +1,15 @@
+def isUnique(inputString):
+    if len(inputString) > 128:
+        return False
+    charSet = set()
+    for c in inputString:
+        if c in charSet:
+            return False
+        else:
+            charSet.add(c)
+    return True
+    
+
+if __name__ == '__main__':
+    inputString = input("Enter the string >> \t")
+    print("Does the string have Unique characters >> " + str(isUnique(inputString)))
